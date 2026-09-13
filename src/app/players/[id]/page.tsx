0aced7f696ad0233df410ffab1eedabee41750b0
Tudino2005 <sasella3644@gmail.com>
2026-09-13 19:57:03 +0200

    style(ui): ripristinata maiuscola iniziale per il nome del partner ideale

--- src/app/players/[id]/page.tsx
+++ src/app/players/[id]/page.tsx
@@ -360,8 +360,8 @@ export default async function PlayerProfilePage({ params }: { params: Promise<{ 
                     <span className="uppercase text-white">
                       PARTNER IDEALE:
                     </span>
-                    <span className="text-yellow-400 normal-case lowercase drop-shadow-md text-3xl ml-1">
-                      {suggestedPartner.partner.name.toLowerCase()}
+                    <span className="text-yellow-400 normal-case drop-shadow-md text-3xl ml-1 capitalize">
+                      {suggestedPartner.partner.name}
                     </span>
                   </h3>
                 </div>
